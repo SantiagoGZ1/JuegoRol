4471872fb1b19a28f5738b94569cb82be0b98d6f
SantiagoG <santi10gz10@gmail.com>
2024-05-15 12:26:46 -0500

    warrior powers

--- src/Charecters/Robot.java
+++ src/Charecters/Robot.java
@@ -1,15 +1,18 @@
 package Charecters;
 
 public class Robot extends Character {
-
-  public Robot(String personaje, String name, int hp, int attackP, int armor, int blades) {
+  private int gun;
+  private int mechaniclArm;
+  private int rocket;
+  public Robot(String personaje, String name, int hp, int attackP, int armor, int gun, int mechaniclArm, int rocket) {
     super(personaje, name, hp, attackP, armor);
-
+    this.gun = gun;
+    this.mechaniclArm = mechaniclArm;
+    this.rocket = rocket;
   }
 
-  public void attack(){
-
-
+  public void attack(Character enemy, int weapon){
+    System.out.println("");
   }
   public void specialAttack(){
   }
